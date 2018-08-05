9bb41099cedc3ae7ba4f1dc07f9cfdd5839cfa35
1 <ppark9553@gmail.com>
2018-08-05 18:04:13 +0900

    ejs 추가 완료

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var PORT = 8080;
 var HOST = '0.0.0.0';
 
 var app = express();
+app.set('views', __dirname + '/templates'); // HTML 파일 연결
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(__dirname + '/assets')); // 정적 파일 연결
 
 app.listen(PORT, HOST);
 console.log('서버가 http://127.0.0.1:' + PORT + '에서 작동하고 있습니다.');
+
+
+app.get('/', (req, res) => {
+  res.render('home.html');
+});
